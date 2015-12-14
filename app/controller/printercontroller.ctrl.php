e6067486c4c2c34428ac2822e5577c79e638930a
Alan Zard <tech@alanzard.com>
2015-12-14 22:25:25 +0100

    PDF prettyfication

--- app/controller/printercontroller.ctrl.php
+++ app/controller/printercontroller.ctrl.php
@@ -19,11 +19,20 @@
             if($type == 'pdf'){ 
             $content = '<style type="text/css">
                         <!--
-                            h3 {background: #e3e3e3;  padding:5mm 8mm;  font-weight: bold; width: 100%;   }
-                            p {background: #FFFFFF;  padding:3mm; }
+                            page {  }
+                            h1 { font-size: 16mm; text-align: left; color: yellowgreen; font-weight: 700; margin: 10mm 0mm 5mm 0mm; }
+                            h2 {color: #042a2b; font-weight: bold; margin: 5mm 0mm; }
+                            h3 {  color: #042a2b; font-weight: normal; margin: 5mm 0mm 2mm; }
+                            p { background: #FFFFFF; line-height: 140%; color: #042a2b;   }
+                            p.small { font-size: 3mm; color: #666; margin: 8mm 0mm;  }
+                            p.small a { color: #666;  }
                         -->
                         </style>
-                        <page>
+                        <page backtop="7mm" backbottom="7mm" backleft="10mm" backright="10mm">
+                         
+                            <p class="small">Produced by the Tool Selection Assistant, (<a href="https://toolselect.theengineroom.org">https://toolselect.theengineroom.org</a>) a guide that helps choose the right tool for transparency and accountability projects. </p>
+                            
+                        
                         <h1>' . $data['title'] . '</h1>';
             }
             else {
@@ -34,7 +43,7 @@
             
             switch($step){
                 case 1:
-                    
+                    $content .= '<h2>Understand Your Needs</h2>';
                     $thisSlide = $Slide->findSlide($project, $step, 3);
                     //dbga($thisSlide);
                     $content .= '   <h3>Description of the project:</h3>
@@ -109,7 +118,7 @@
                                                 
                     break;
                 case 2:
-                    
+                    $content .= '<h2>Understand The Tech</h2>';
                     $thisSlide = $Slide->findSlide($project, $step, 2);            
                     $content .= '   <h3>What things the tool must be able to do:</h3>
                                     <p>' .
@@ -141,6 +150,7 @@
                     
                     break;
                 case 3:
+                    $content .= '<h2>Try it out!</h2>';
                     $thisSlide = $Slide->findSlide($project, $step, 4);            
                     $content .= '   <h3>Methods for trialling the tool(s):</h3>
                                     <p>' .
@@ -166,7 +176,7 @@
                     break;
                 
                 case 4:
-                    
+                    $content .= '<h2>Find help</h2>';
                     
                     $thisSlide = $Slide->findSlide($project, 4, 2);            
                     $content .= '   <h3>Areas where support is needed:</h3>
@@ -220,6 +230,7 @@
                 case 'all':
                     
                     /**  BOF STEP 1 **/
+                    $content .= '<h2>Understand Your Needs</h2>';
                     $thisSlide = $Slide->findSlide($project, 1, 3);
                     //dbga($thisSlide);
                     $content .= '   <h3>Description of the project:</h3>
@@ -291,6 +302,7 @@
                                     '</p>';
                     /** EOF STEP 1 **/
                     /** BOF STEP 2 **/
+                    $content .= '<h2>Understand the Tech</h2>';
                     $thisSlide = $Slide->findSlide($project, 2, 2);            
                     $content .= '   <h3>What things the tool must be able to do:</h3>
                                     <p>' .
@@ -321,7 +333,7 @@
                                     '</p>';
                     /** EOF STEP 2 **/
                     /** BOF STEP 3 **/
-                    
+                    $content .= '<h2>Try it out!</h2>';
                     $thisSlide = $Slide->findSlide($project, 3, 4);            
                     $content .= '   <h3>Methods for trialling the tool(s):</h3>
                                     <p>' .
@@ -348,7 +360,7 @@
                     
                     
                     /** BOF STEP 4 **/
-                    
+                    $content .= '<h2>Find Help</h2>';
                     $thisSlide = $Slide->findSlide($project, 4, 2);            
                     $content .= '   <h3>Areas where support is needed:</h3>
                                     <p>' .
@@ -400,11 +412,10 @@
                         
             
             //echo $content;
-            
+            $content .= '<p class="small">The guide was built by the engine room (<a href="https://www.theengineroom.org">https://www.theengineroom.org</a>), Pawa254 (<a href="http://pawa254.org">http://pawa254.org</a>) and the Network Society Lab (<a href="http://www.networksociety.co.za">www.networksociety.co.za</a>) at the University of the Witwatersrand. It is part of a research project supported by Making All Voices Count (<a href="http://www.makingallvoicescount.org/">www.makingallvoicescount.org/</a>)</p>';
+                
             if($type == 'pdf'){
-                
                 $content .= '</page>';
-                
                 $html2pdf = new HTML2PDF('P','A4','en');
                 $html2pdf->WriteHTML($content);
                 $html2pdf->Output('TSA-Step-' . $step . '.pdf');
