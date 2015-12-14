12eb1ff99932636c3b4af41e2e2b56eb0265291f
Alan Zard <tech@alanzard.com>
2015-12-14 09:39:58 +0100

    New Homepage WIP

--- app/view/page/home.php
+++ app/view/page/home.php
@@ -27,15 +27,59 @@
             </div>
         </div>
     </div>
+    <div class="container" id="text-snaps">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3 text-snap-title"><h3>What is it</h3></div>
+            <div class="col-xs-12 col-sm-6 col-md-3"><p><strong>A 4-step guided process</strong> that will help you choose your best option.</p></div>
+            <div class="col-xs-12 col-sm-6 col-md-3 text-snap-title"><h3>What I'll get</h3></div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <ul>
+                    <li>Real life <strong>examples</strong></li>
+                    <li>Tips</li>
+                    <li>Links to <strong>external resources</strong> for help</li>
+                    <li>A <strong>structured report</strong> of all your decisions.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" id="walkthrough">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-md-3 step step-1">
+                <div class="step-inner-wrap">
+                    <span class="step-indicator"></span>
+                    <span class="step-description">Understand your needs</span>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 step step-2">
+                <div class="step-inner-wrap">
+                    <span class="step-indicator"></span><span class="step-indicator"></span>
+                    <span class="step-description">Understand the tech</span>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 step step-3">
+                <div class="step-inner-wrap">
+                    <span class="step-indicator"></span><span class="step-indicator"></span><span class="step-indicator"></span>
+                    <span class="step-description">Try it out!</span>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 step step-4">
+                <div class="step-inner-wrap">
+                    <span class="step-indicator"></span><span class="step-indicator"></span><span class="step-indicator"></span><span class="step-indicator"></span>
+                    <span class="step-description">Find help or learn</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center" id="quick-tips">
+        <p>Don’t have time to read the guide? Try our <a href="/page/quick-tips">quick tips</a> first.</p>
+    </div>
+    
+    <?php if (!empty($page)) { ?> 
     <div class="container">
         <section class="row">
             <div class="col-md-12">            
                 <?php echo $page; ?>
-                <p class="text-center">
-                    <a class="btn btn-alt btn-lg" href="/user/projects"><i class="fa fa-hand-o-right"></i> Start planning now!</a>
-                    <br /><br />
-                    Don’t have time to read the guide? Try our <a href="/page/quick-tips">quick tips</a> first.
-                </p>
             </div>
         </section>
     </div>
+    <?php } ?>
