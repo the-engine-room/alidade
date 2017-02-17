8bf8199a8b109c85789e734320a43f3425812a10
Yavin <tech@alanzard.com>
2017-02-17 08:34:03 +0100

    Added controls on leaving the tool without saving

--- public/dist/js/ToolSelectionAssistant.js
+++ public/dist/js/ToolSelectionAssistant.js
@@ -147,6 +147,25 @@ $(document).ready(function(){
 
     $('#slide-sidebar').css({ 'min-height': $('#slide-content').outerHeight() + 50 });
 
+    /** warn of data loss **/
+    if($('#slide-page').length > 0){
+      /** what have you clicked? **/
+      $(window).click(function(ev){
+        var target = ev.target;
+        console.log($(target));
+        console.log($(target).parents('#slide-page').length == 0);
+        /** if i'm clicking stuff outside of the tool execute onbefroeunload warning **/
+        window.onbeforeunload = function (e) {
+          e = e || window.event;
+          var y = e.pageY || e.clientY;
+          if (y < 0 || $(target).parents('#slide-page').length == 0){
+            return "Leaving now will result in loosing any unsaved progress.";
+          }
+        }
+      });
+    }
+
+
     /** Set arrowheads for the sidebar **/
     $('.step ul li a').each(function(){
         if($(this).height() > 30) {
@@ -154,14 +173,7 @@ $(document).ready(function(){
         }
     });
 
-    /** alert out if window close **/ /*
-    if($('#slide-page').length > 0) {
 
-          window.onbeforeunload = function(){
-            return confirm('Are you sure? All unsaved progress will be lost!');
-          }
-        }
-      */
     /** Manage the Selection on 1.3 **/
     if( $('input[name="extra"]').length > 0 && $('input[name="extra"]').val() == 'no') {
       $('.picks').addClass('hide');
